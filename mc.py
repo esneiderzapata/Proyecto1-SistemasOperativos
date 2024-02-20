a1d7bb52995f0d5f0d7656758b6c077a942d4f90
Santiago Molina Mejia <santi.lds17@gmail.com>
2024-02-20 07:04:40 -0500

    prueba
    paralelismo

--- mc.py
+++ mc.py
@@ -1,27 +1,38 @@
 import argparse
+import logging
+import multiprocessing
 import os
+import time
+from multiprocessing import Process, Pool
+
+
 from pydub import AudioSegment
 from io import BytesIO
+
 
 def convertir_a_mp3(audio_cd):
     output = BytesIO()
     audio_cd.export(output, format="mp3")
     return output.getvalue()
 
+
 def convertir_a_ogg(audio_cd):
     output = BytesIO()
     audio_cd.export(output, format="ogg")
     return output.getvalue()
+
 
 def convertir_a_flac(audio_cd):
     output = BytesIO()
     audio_cd.export(output, format="flac")
     return output.getvalue()
 
+
 def guardar_archivo(nombre_archivo, contenido):
     with open(nombre_archivo, "wb") as f:
         f.write(contenido)
     print(f"Archivo guardado como {nombre_archivo}")
+
 
 def convertir_carpeta(carpeta, formato):
     contador = 0
@@ -38,14 +49,17 @@ def convertir_carpeta(carpeta, formato):
                 guardar_archivo(f"archivo {contador}.flac", flac_content)
             contador += 1
 
+
 def main():
+    start_time = time.time()
     # Crear y configurar Parser para obtener argumentos
     parser = argparse.ArgumentParser(description="---")
     parser.add_argument("-f", "--file", required=True, help="Ruta al archivo AIFF a convertir o a la carpeta")
-    parser.add_argument("-e", "--encoding", type=str, choices=["mp3", "ogg", "flac"], help="Formato al que se desea convertir (mp3, ogg, flac)", required=False)
+    parser.add_argument("-e", "--encoding", type=str, choices=["mp3", "ogg", "flac"],
+                        help="Formato al que se desea convertir (mp3, ogg, flac)", required=False)
     args = parser.parse_args()
 
-    #Comprobar si file es una carpeta
+    # Comprobar si file es una carpeta
     if os.path.isdir(args.file):
         if (args.encoding != None):
             convertir_carpeta(args.file, args.encoding)
@@ -56,15 +70,36 @@ def main():
     # Si es un archivo .aif cargarlo
     audio_cd = AudioSegment.from_file(args.file, format="aiff")
 
-    # Convertir a MP3, AAC y FLAC
+    multiprocessing.log_to_stderr()
+    logging.basicConfig(level=logging.INFO)
+
+    # with Pool(processes=3) as pool:
+    #     mp3_result = pool.apply_async(convertir_a_mp3, (audio_cd,))
+    #     ogg_result = pool.apply_async(convertir_a_ogg, (audio_cd,))
+    #     flac_result = pool.apply_async(convertir_a_flac, (audio_cd,))
+    #
+    #     mp3_content = mp3_result.get()
+    #     ogg_content = ogg_result.get()
+    #     flac_content = flac_result.get()
+
+
+
+
+    #Convertir a MP3, AAC y FLAC
     mp3_content = convertir_a_mp3(audio_cd)
     ogg_content = convertir_a_ogg(audio_cd)
     flac_content = convertir_a_flac(audio_cd)
 
-    # Calcular Pesos de cada archivo
-    sizeMP3 = len(mp3_content)/1024/1024
-    sizeOGG = len(ogg_content)/1024/1024
-    sizeFLAC = len(flac_content)/1024/1024
+    end_time = time.time()  # Finalizar el contador de tiempo
+    print(f"Tiempo transcurrido: {end_time - start_time} segundos")
+
+    #Calcular Pesos de cada archivo
+    sizeMP3 = len(mp3_content) / 1024 / 1024
+    sizeOGG = len(ogg_content) / 1024 / 1024
+    sizeFLAC = len(flac_content) / 1024 / 1024
+
+
+
 
     # Mostrar opciones al usuario
     print("\nOpciones disponibles:")
@@ -83,12 +118,13 @@ def main():
     else:
         print("Opción no válida")
 
+
 if __name__ == "__main__":
     main()
 
-#SE NECESITA INSTALAR PYDUB CON: pip install pydub
-#SE NECESITA INSTALAR FFmpeg Y AGREGARLO AL PATH
-#PRIMER ERROR: NO FUNCIONA LA CONVERSIÓN A M4A (ACC) SOLUCIÓN USAR FORMAT="adts"
-#MP3 AL SER EL FORMATO MÁS POPULAR Y COMPATIBLE DE AUDIO
-#OGG AL SER EL FORMATO CON MENOR PESO
-#FLAC AL SER EL FORMATO QUE ES UNA REPLICA EXACTA DEL AUDIO
+# SE NECESITA INSTALAR PYDUB CON: pip install pydub
+# SE NECESITA INSTALAR FFmpeg Y AGREGARLO AL PATH
+# PRIMER ERROR: NO FUNCIONA LA CONVERSIÓN A M4A (ACC) SOLUCIÓN USAR FORMAT="adts"
+# MP3 AL SER EL FORMATO MÁS POPULAR Y COMPATIBLE DE AUDIO
+# OGG AL SER EL FORMATO CON MENOR PESO
+# FLAC AL SER EL FORMATO QUE ES UNA REPLICA EXACTA DEL AUDIO
