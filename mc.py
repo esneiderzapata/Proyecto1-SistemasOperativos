119e5dc04da485c39e1253e16b7ce51420e1cab4
Santiago Molina Mejia <santi.lds17@gmail.com>
2024-02-21 11:09:42 -0500

    paralelismo carpetas

--- mc.py
+++ mc.py
@@ -1,11 +1,8 @@
 import argparse
-import logging
-import multiprocessing
 import os
 import time
-from multiprocessing import Process, Pool
 
-
+from multiprocessing import Pool
 from pydub import AudioSegment
 from io import BytesIO
 
@@ -34,20 +31,28 @@ def guardar_archivo(nombre_archivo, contenido):
     print(f"Archivo guardado como {nombre_archivo}")
 
 
+def procesar_formato(archivo, nombre_archivo, formato):
+    print(f"Procesando {nombre_archivo}")
+    if formato == "mp3":
+        mp3_content = convertir_a_mp3(archivo)
+        guardar_archivo(f"{nombre_archivo}.mp3", mp3_content)
+    if formato == "ogg":
+        ogg_content = convertir_a_ogg(archivo)
+        guardar_archivo(f"{nombre_archivo}.ogg", ogg_content)
+    if formato == "flac":
+        flac_content = convertir_a_flac(archivo)
+        guardar_archivo(f"{nombre_archivo}.flac", flac_content)
+
+
 def convertir_carpeta(carpeta, formato):
-    contador = 0
-    for archivo in os.listdir(carpeta):
-        if archivo.endswith(".aif"):
-            if formato == "mp3":
-                mp3_content = convertir_a_mp3(AudioSegment.from_file(os.path.join(carpeta, archivo), format="aiff"))
-                guardar_archivo(f"archivo {contador}.mp3", mp3_content)
-            elif formato == "ogg":
-                ogg_content = convertir_a_ogg(AudioSegment.from_file(os.path.join(carpeta, archivo), format="aiff"))
-                guardar_archivo(f"archivo {contador}.ogg", ogg_content)
-            elif formato == "flac":
-                flac_content = convertir_a_flac(AudioSegment.from_file(os.path.join(carpeta, archivo), format="aiff"))
-                guardar_archivo(f"archivo {contador}.flac", flac_content)
-            contador += 1
+    start_time = time.time()
+    archivo = [archivo for archivo in os.listdir(carpeta) if archivo.endswith("aif")]
+    print(archivo)
+    with Pool() as pool:
+        pool.starmap(procesar_formato, [(AudioSegment.from_file(os.path.join(carpeta, archivo), format="aiff"),
+                                         archivo, formato) for archivo in archivo])
+    end_time = time.time()  # Finalizar el contador de tiempo
+    print(f"Tiempo transcurrido: {end_time - start_time} segundos")
 
 
 def main():
@@ -61,7 +66,7 @@ def main():
 
     # Comprobar si file es una carpeta
     if os.path.isdir(args.file):
-        if (args.encoding != None):
+        if args.encoding is not None:
             convertir_carpeta(args.file, args.encoding)
         else:
             print("No se sabe a que formato convertir la carpeta")
@@ -70,36 +75,29 @@ def main():
     # Si es un archivo .aif cargarlo
     audio_cd = AudioSegment.from_file(args.file, format="aiff")
 
-    multiprocessing.log_to_stderr()
-    logging.basicConfig(level=logging.INFO)
+    with Pool(processes=3) as pool:
+        print(audio_cd)
 
-    # with Pool(processes=3) as pool:
-    #     mp3_result = pool.apply_async(convertir_a_mp3, (audio_cd,))
-    #     ogg_result = pool.apply_async(convertir_a_ogg, (audio_cd,))
-    #     flac_result = pool.apply_async(convertir_a_flac, (audio_cd,))
-    #
-    #     mp3_content = mp3_result.get()
-    #     ogg_content = ogg_result.get()
-    #     flac_content = flac_result.get()
+        mp3_result = pool.apply_async(convertir_a_mp3, (audio_cd,))
+        ogg_result = pool.apply_async(convertir_a_ogg, (audio_cd,))
+        flac_result = pool.apply_async(convertir_a_flac, (audio_cd,))
 
+        mp3_content = mp3_result.get()
+        ogg_content = ogg_result.get()
+        flac_content = flac_result.get()
 
-
-
-    #Convertir a MP3, AAC y FLAC
-    mp3_content = convertir_a_mp3(audio_cd)
-    ogg_content = convertir_a_ogg(audio_cd)
-    flac_content = convertir_a_flac(audio_cd)
+    # Convertir a MP3, AAC y FLAC
+    # mp3_content = convertir_a_mp3(audio_cd)
+    # ogg_content = convertir_a_ogg(audio_cd)
+    # flac_content = convertir_a_flac(audio_cd)
 
     end_time = time.time()  # Finalizar el contador de tiempo
     print(f"Tiempo transcurrido: {end_time - start_time} segundos")
 
-    #Calcular Pesos de cada archivo
+    # Calcular Pesos de cada archivo
     sizeMP3 = len(mp3_content) / 1024 / 1024
     sizeOGG = len(ogg_content) / 1024 / 1024
     sizeFLAC = len(flac_content) / 1024 / 1024
-
-
-
 
     # Mostrar opciones al usuario
     print("\nOpciones disponibles:")
